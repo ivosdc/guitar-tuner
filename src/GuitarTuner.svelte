4bd790939b30f11f1010f1cc0902b25d4a362da8
ivosdc <ibozic@mail.de>
2022-03-25 17:47:52 +0100

    adds information to canvas

--- src/GuitarTuner.svelte
+++ src/GuitarTuner.svelte
@@ -6,6 +6,8 @@ import {pitchDetection, pitchToNote, detuneFromPitch, getNoteString} from './pit
 let pitch = -1;
 let note = '';
 let device = '';
+let detune = 0;
+let canvas;
 
 onMount(async () => {
     const stream = await navigator.mediaDevices.getUserMedia({audio: true}).catch(err => {
@@ -21,14 +23,33 @@ onMount(async () => {
     const microphone = aCtx.createMediaStreamSource(stream);
     microphone.connect(analyser);
     let fData = new Float32Array(analyser.frequencyBinCount);
-
     (function updateCanvas() {
+        const ctx = canvas.getContext("2d");
         analyser.getFloatTimeDomainData(fData);
         pitch = pitchDetection(fData, aCtx.sampleRate);
         note =  pitchToNote(pitch);
+        detune = detuneFromPitch(pitch, note);
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(0, 0, 200, 100);
+        ctx.fillStyle = "rgb(0, 0, 0)";
+        ctx.font = "9px Arial"
+        ctx.fillText(showDevice(device), 1, 99);
+        ctx.font = "12px Arial";
+        ctx.fillText("Hz: " + showHz(pitch), 5, 15);
+        ctx.fillText(showDetune(pitch, note), 90, 15);
+        ctx.font = "30px Arial";
+        ctx.fillText(showNote(note), 90, 80);
+        ctx.beginPath();
+        ctx.moveTo(100, 100);
+        ctx.lineTo(100 + detune, 0);
+        ctx.stroke();
+        ctx.closePath();
         requestAnimationFrame(updateCanvas);
     }());
+
 })
+
+
 
 function showNote(note) {
     let notevalue = '';
@@ -41,7 +62,7 @@ function showNote(note) {
 function showDetune(pitch, note) {
     let detune = '';
     if (note) {
-        detune = " detune: " + detuneFromPitch(pitch, note);
+        detune = detuneFromPitch(pitch, note);
     }
     return detune;
 }
@@ -50,21 +71,12 @@ function showHz(pitch) {
     return pitch === -1 ? 'no signal' : Math.round(pitch);
 }
 
+function showDevice(device) {
+    let offset = device.lastIndexOf("(");
+    return device.substr(0, offset - 1);
+}
+
 </script>
 
-
-<main>
-    <p id="device">Using device: {device}</p>
-    <p id="pitch">Hz: {showHz(pitch)}</p>
-    <span id="note">{showNote(note)}</span>
-    <span id="detune">{showDetune(pitch, note)}</span>
-</main>
-
-<style>
-    #note {
-        font-size: 3em;
-    }
-    #detune {
-        font-size: 1.5em;
-    }
-</style>
+<canvas bind:this={canvas} width="200" height="100" style="border:1px solid #000000;">
+</canvas>
